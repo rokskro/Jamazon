e5c590b8e3b90f2dfe03d09de0c538686f4d9615
rox <roksana.krogulec@synehcron.com>
2024-01-29 15:58:14 +0000

    product repo fix

--- src/main/java/com/testing/service/ProductRepository.java
+++ src/main/java/com/testing/service/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.testing.service;
 
+import com.testing.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository <Product, Long> {
