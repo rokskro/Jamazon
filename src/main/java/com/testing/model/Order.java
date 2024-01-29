e66dd8b3075e4d06b97999403b8c03fac663ae16
Conorrkk <conorrkk@icloud.com>
2024-01-29 14:30:39 +0000

    Order and product
    Order and product classes

--- src/main/java/com/testing/model/Order.java
+++ src/main/java/com/testing/model/Order.java
@@ -1,4 +1,62 @@
 package com.testing.model;
 
+import java.util.HashMap;
+
 public class Order {
+    Long orderId;
+    HashMap<Product, Integer> productList = new HashMap<Product, Integer>();
+    Long address;
+    double totalCost;
+    String datePlaced;
+
+    public Order(){
+
+    }
+
+    public Order(Long orderId, HashMap<Product, Integer> productList, Long address, double totalCost, String datePlaced) {
+        this.orderId = orderId;
+        this.productList = productList;
+        this.address = address;
+        this.totalCost = totalCost;
+        this.datePlaced = datePlaced;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public HashMap<Product, Integer> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(HashMap<Product, Integer> productList) {
+        this.productList = productList;
+    }
+
+    public Long getAddress() {
+        return address;
+    }
+
+    public void setAddress(Long address) {
+        this.address = address;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public String getDatePlaced() {
+        return datePlaced;
+    }
+
+    public void setDatePlaced(String datePlaced) {
+        this.datePlaced = datePlaced;
+    }
+
 }
--- src/main/java/com/testing/model/Product.java
+++ src/main/java/com/testing/model/Product.java
@@ -1,4 +1,70 @@
 package com.testing.model;
 
 public class Product {
+    Long productId;
+    String productName;
+    double value;
+    String description;
+    String image;
+    int quantity;
+
+    public Product() {
+    }
+
+    public Product(Long productId, String productName, double value, String description, String image, int quantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.value = value;
+        this.description = description;
+        this.image = image;
+        this.quantity = quantity;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
