65080207673f88427655e3d4df5267b6fc6f4acc
rox <roksana.krogulec@synehcron.com>
2024-01-29 11:28:36 +0000

    Initial Commit

---
+++ src/main/java/com/testing/JamazonApplication.java
@@ -0,0 +1,12 @@
+package com.testing;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamazonApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Jamazon");
+	}
+
+}
