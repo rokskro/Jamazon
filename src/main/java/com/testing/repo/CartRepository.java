62612547ec8668c605d3ddff328b7501a9052015
l-gourley <lgourley04@qub.ac.uk>
2024-01-30 16:46:28 +0000

    Cart added
    Cart Repo, Dto

---
+++ src/main/java/com/testing/repo/CartRepository.java
@@ -0,0 +1,11 @@
+package com.testing.repo;
+
+
+import com.testing.model.pojos.Customer;
+import com.testing.model.pojos.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Product, Long> {
+}
