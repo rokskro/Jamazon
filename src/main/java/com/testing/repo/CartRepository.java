5662c31aa6fb99fd7ea94ee1d725805cfd296fa3
l-gourley <lgourley04@qub.ac.uk>
2024-01-31 11:51:26 +0000

    Cart updates
    Cart updates (incomplete)

--- src/main/java/com/testing/repo/CartRepository.java
+++ src/main/java/com/testing/repo/CartRepository.java
@@ -1,11 +1,11 @@
 package com.testing.repo;
 
 
-import com.testing.model.pojos.Product;
+import com.testing.model.pojos.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository extends JpaRepository<Product, Long> {
+public interface CartRepository extends JpaRepository<Cart, Long> {
 
 }
