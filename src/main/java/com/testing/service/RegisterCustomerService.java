1a553c4593aec5a2217a1be273afac8a4e5bc1c6
SeñorFunkTheGreat <bigmanwhatisup224@gmail.com>
2024-01-31 12:06:29 +0000

    Added some login stuff, not finished, still have to test

--- src/main/java/com/testing/service/RegisterCustomerService.java
+++ src/main/java/com/testing/service/RegisterCustomerService.java
@@ -6,6 +6,7 @@ import com.testing.model.pojos.Customer;
 import com.testing.repo.CustomerRepository;
 import jdk.internal.org.jline.terminal.TerminalBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -19,6 +20,12 @@ public class RegisterCustomerService {
 
     public String registerCustomer(RegisterCustomerDto registerCustomerDto){
         Customer customer = new ObjectMapper().convertValue(registerCustomerDto, Customer.class);
+        
+        //Encoding password so that in database it is hash value is stored, improved security
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(customer.getPassword());
+        customer.setPassword(encodedPassword);
+        
         Long customerId = customerRepo.save(customer).getCustomerId();
 
         return "Customer registered successfully: " + customerId;
