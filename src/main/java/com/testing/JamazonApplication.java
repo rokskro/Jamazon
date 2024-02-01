e60673de927361086d5ac6a3fe14d5c06620cc56
SeñorFunkTheGreat <bigmanwhatisup224@gmail.com>
2024-02-01 13:01:57 +0000

    Test

--- src/main/java/com/testing/JamazonApplication.java
+++ src/main/java/com/testing/JamazonApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JamazonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JamazonApplication.class, args);	}
+		SpringApplication.run(JamazonApplication.class, args);	
+		}
 
 }
